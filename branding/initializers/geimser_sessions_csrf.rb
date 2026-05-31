e06b7c4d482dfde1343512af0cdd215ecbee7253
hhdev2026 <hhdev2026@gmail.com>
2026-05-31 10:55:04 -0400

    Allow proxied Zammad login bootstrap

---
+++ branding/initializers/geimser_sessions_csrf.rb
@@ -0,0 +1,12 @@
+# Zammad 7 can reject the public sign-in flow behind the HTTPS front proxy when
+# the SPA has no persisted session yet. Keep CSRF protection everywhere else,
+# but let the unauthenticated bootstrap/login endpoints issue the first session.
+Rails.application.config.to_prepare do
+  next unless defined?(SessionsController)
+
+  SessionsController.skip_before_action(
+    :verify_authenticity_token,
+    only: %i[show signin],
+    raise: false,
+  )
+end
