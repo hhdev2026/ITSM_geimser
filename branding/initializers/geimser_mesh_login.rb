d45f654f7f43b73e4af0ca532a7a54648239e5d7
hhdev2026 <hhdev2026@gmail.com>
2026-05-31 18:22:28 -0400

    Install Mesh login as Zammad route

--- branding/initializers/geimser_mesh_login.rb
+++ branding/initializers/geimser_mesh_login.rb
@@ -1,5 +1,1 @@
-Rails.application.routes.append do
-  get '/geimser/mesh/login', to: 'geimser_mesh_login#show'
-end
-
-Rails.application.reload_routes!
+# The Mesh login route is installed under config/routes in the custom image.
---
+++ branding/routes/geimser_mesh_login.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get '/geimser/mesh/login', to: 'geimser_mesh_login#show'
+end
